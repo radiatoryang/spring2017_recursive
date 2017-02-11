7d07e62f56b2e54aaf46b9e714a539b578617ff3
Robert Yang <yang.robert.w@gmail.com>
2017-02-10 23:50:36 -0500

    week02 (intro to Unity) and week03 (intro to code)... CubeMove.cs and FirstPersonLook.cs

---
+++ week03_introToCode/Assets/CubeMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World"); // print to Console
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Bonjour Monde");
+
+		// transform.position += new Vector3( Random.Range(-0.1f, 0.1f), 0.1f, 0f );
+
+		// move forward (based on way we're looking)
+		transform.Translate( 0f, 0f, 0.1f ); 
+
+		// rotate randomly on Y axis (green)
+		transform.Rotate( 0f, Random.Range(-45f, 45f), 0f );
+
+	}
+
+}
---
+++ week03_introToCode/Assets/FirstPersonLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour {
+	
+	void Update () {
+		// for Mouse X: -1 if moving left, 0 if not moving, +1 if move right
+		float mouseHorizontal = Input.GetAxis("Mouse X");
+		float mouseVertical = Input.GetAxis("Mouse Y");
+
+		// rotate the object (maybe the Main Camera?) based on mouse movement
+		transform.Rotate( -mouseVertical, mouseHorizontal, 0f );
+
+		// un-roll the camera
+		transform.eulerAngles = new Vector3( transform.eulerAngles.x, transform.eulerAngles.y, 0f );
+		// transform.eulerAngles.z = 0f; // THIS WILL NOT WORK, YOU CANNOT CHANGE X Y Z DIRECTLY
+
+		// if you hold down LEFT MOUSE BUTTON, you move forward
+		if ( Input.GetMouseButton(0) == true ) { // "0" = left-click, "1" = right-click
+			transform.Translate( 0f, 0f, 0.1f ); // move forward based on my rotation
+			Cursor.lockState = CursorLockMode.Locked; // lock the cursor in middle of screen
+			Cursor.visible = false; // hide the cursor 
+		}
+
+	}
+}
