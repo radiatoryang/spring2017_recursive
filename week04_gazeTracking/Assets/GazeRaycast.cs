d4126187a149ef6390d8d5bc0eba7d4b3b991346
Robert Yang <yang.robert.w@gmail.com>
2017-02-17 20:37:49 -0500

    week04 (gaze tracking via raycast)... see GazeRaycast.cs code

---
+++ week04_gazeTracking/Assets/GazeRaycast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this script on the thing to be looked at
+// do not put on the player or camera!!
+public class GazeRaycast : MonoBehaviour {
+
+	float timeLookedAt = 0f; // number of seconds we looked at this thing
+	
+	void Update () {
+		// 1. calculate the Raycast origin and direction
+		Ray ray = new Ray( Camera.main.transform.position, Camera.main.transform.forward );
+
+		// 2. setup our RaycastHit variable, reserve memory for it
+		RaycastHit myRayHit = new RaycastHit();
+
+		Debug.DrawRay( ray.origin, ray.direction * 50f );
+
+		// 3. test our Raycast
+		if ( Physics.Raycast( ray, out myRayHit, 50f ) ) {
+			// 4. did the Raycast hit this object?
+			if ( myRayHit.collider == GetComponent<Collider>() ) {
+				// this is up to you... what should we do if we look at it?
+				Debug.Log("raycast hit on " + gameObject.name );
+				timeLookedAt += Time.deltaTime; // "deltaTime" is how long the frame was, after 1 sec it will = 1
+				if ( timeLookedAt >= 1f ) { // did we look for 1+ seconds?
+					timeLookedAt = 0f;
+					// now do something? play a sound, play animation, explode, etc....
+					Destroy( gameObject ); // delete this after 1 second
+				}
+			} else {
+				timeLookedAt = 0f; // reset to 0 if we look at a different collider
+			}
+		} else {
+			timeLookedAt = 0f; // reset to 0 if we look at NO colliders at all
+		}
+	}
+
+}
---
+++ week04_gazeTracking/Assets/MouseLook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseHorizontal = Input.GetAxis("Mouse X"); // horizontal mouse movement
+		float mouseVertical = Input.GetAxis("Mouse Y"); // vertical mouse movement
+
+		// rotate the camera based on mouse speed
+		transform.Rotate( -mouseVertical, mouseHorizontal, 0f );
+
+		// un-roll the camera by making sure Z is always zero, while preserving X and Y
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
+
+		// transform.eulerAngles.z = 0f; // this does not WORK
+	}
+}
